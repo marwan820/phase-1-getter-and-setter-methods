e385ff4754fb114c182bab5930e262a577a72e20
Marwan Rahim <marwan820@Marwan>
2023-06-01 17:25:42 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Add your Circle class here
+// create a class of Circle
+// radius parameter
+// use MATH.PI for pi
+//define getter methods for -diameter
+//                          -circumference
+//                          - area 
+// which will calculate each value using this. and pi
+// setter for the same getter methods 
+// Math.sqrt() for area setter 
+
+class Circle {
+    constructor(radius){
+        this.radius = radius 
+    }
+   get diameter(){
+    return this.radius * 2
+   }
+   get circumference(){
+     return Math.PI * this.diameter
+   }
+   
+   get area (){
+     return Math.PI * (this.radius **2)
+   } 
+   set diameter(newDiameter){
+     this.radius= newDiameter/2
+   }
+   set circumference(newCircumference){
+    this.radius = newCircumference/(Math.PI *2)
+   }
+   set area(newArea){
+    this.radius = Math.sqrt(newArea/Math.PI)
+   }
+   
+
+    
+
+    
+   }
+    let circle = new Circle(10)
+
+    //console.log(circle.diameter)
+
+    circle.diameter = 20
+
+    console.log(circle.circumference)
+
+
+
+
+
